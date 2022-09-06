f90f67604a30b4eb5f134c5b9bba608a74fe3f48
barry503 <momar5618@gmail.com>
2022-09-06 00:02:02 -0600

    cambie un # por // de una url

---
+++ modelos/m_permiso.php
@@ -0,0 +1,115 @@
+<?php 
+/***********************************
+*version: 1                        *
+*fecha: 02-09-2022                 *
+*Dev: https://github.com/barry503  *
+**********************************/
+//incluimos inicialmente la conexion ala base de datos
+require '../config/conexion.php';
+
+class Permiso
+{
+
+// inplementamos nuestro constructor
+	public function __construct()
+	{
+      
+
+
+	}
+
+
+
+
+ //inplementar un metodo para insertar una materia
+  public function insertar($nombre)
+  {
+         $sql = "INSERT INTO permiso (nombre)
+         VALUES('$nombre')";
+             return ejecutarConsulta($sql);
+
+  }
+
+
+
+   //inplementar un metodo para editar registros
+public function editar($idpermiso,$nombre)
+	{
+         $sql = "UPDATE permiso SET nombre='$nombre'
+         WHERE idpermiso='$idpermiso' ";
+             return ejecutarConsulta($sql);
+
+	}
+
+
+
+
+
+
+
+
+ //inplementar un metodo para desactivar grado
+public function desactivar($idpermiso)
+{
+  $sql = "UPDATE pm_permiso SET condicion='0' WHERE idpermiso='$idpermiso'";
+    return ejecutarConsulta($sql);
+
+}
+
+
+//inplementar un metodo para activar grado
+public function activar($idpermiso)
+{
+  $sql = "UPDATE pm_permiso SET condicion='1' WHERE idpermiso='$idpermiso'";
+    return ejecutarConsulta($sql);
+
+}
+
+
+
+
+//inplementar un metodo para eliminar permiso
+public function eliminar($idpermiso)
+{
+  $sql = "DELETE FROM pm_permiso WHERE idpermiso='$idpermiso'";
+    return ejecutarConsulta($sql);
+
+}
+
+
+
+
+//inplementar un metodo para mostrar  registros a modificar
+public function mostrar($idpermiso)
+{
+	$sql= "SELECT * FROM pm_permiso WHERE idpermiso='$idpermiso'";
+	return ejecutarConsultaSimpleFila($sql);
+}
+
+
+
+
+
+
+
+
+   
+//inplementar un metodo para listar  registros 
+public function listar()
+{
+	$sql="SELECT * FROM pm_permiso";
+	return ejecutarConsulta($sql);
+
+}
+
+
+
+}
+
+
+
+
+
+
+ ?>
+
